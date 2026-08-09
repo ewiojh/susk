329b03e9a4864aec274b52790cc299b33b7b625c
ewiojh <qwepweqwe@outlook.com>
2026-08-09 19:05:16 +0800

    更新 ppppppp.lua

--- ppppppp.lua
+++ ppppppp.lua
@@ -1,1 +1,11 @@
-hum.WalkSpeed = 999  -- 超快
+-- ====== 超级加速（一键执行） ======
+local Player = game.Players.LocalPlayer
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local Humanoid = Character:FindFirstChild("Humanoid")
+
+if Humanoid then
+    Humanoid.WalkSpeed = 100
+    print("移速已设为100")
+else
+    print("找不到Humanoid")
+end
